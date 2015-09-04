d3c8467cefa435e228fdea60cf19bb5fde88e1f7
Renato Gama <renatoargh@gmail.com>
2015-09-03 23:56:22 -0300

    more+

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var app = require('express')(),
     os = require("os");
 
 app.get('/', function(req, res) {
-    res.end('HOST: ' + os.hostname());
+    res.end([
+        'DATE: ' + new Date().toIsoString(),
+        'HOST: ' + os.hostname()
+    ].join('\n'));
 });
 
 app.listen(80);
